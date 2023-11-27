17e4bfe37354564f8427b7186aee74e992f3f6d3
Ethan <milonthan@gmail.com>
2023-11-27 17:58:51 +0100

    feat: small improvement

--- src/dissector/ip_dissectors.c
+++ src/dissector/ip_dissectors.c
@@ -1,88 +1,138 @@
 #include <netinet/ip.h>
+#include <netinet/ip6.h>
 #include <stddef.h>
 #include <string.h>
 
 #include "dissector.h"
 #include "utils/string.h"
 
-const char *get_protocol(int protocol)
+const char *get_protocol(int protocol, int is_ipv6)
 {
-	if (protocol == IPPROTO_ICMP)
-		return "ICMP";
-	else if (protocol == IPPROTO_IGMP)
-		return "IGMP";
-	else if (protocol == IPPROTO_IPIP)
-		return "IPIP";
-	else if (protocol == IPPROTO_TCP)
-		return "TCP";
-	else if (protocol == IPPROTO_EGP)
-		return "EGP";
-	else if (protocol == IPPROTO_PUP)
-		return "PUP";
-	else if (protocol == IPPROTO_UDP)
-		return "UDP";
-	else if (protocol == IPPROTO_IDP)
-		return "IDP";
-	else if (protocol == IPPROTO_TP)
-		return "TP";
-	else if (protocol == IPPROTO_DCCP)
-		return "DCCP";
-	else if (protocol == IPPROTO_IPV6)
-		return "IPv6";
-	else if (protocol == IPPROTO_RSVP)
-		return "RSVP";
-	else if (protocol == IPPROTO_GRE)
-		return "GRE";
-	else if (protocol == IPPROTO_ESP)
-		return "ESP";
-	else if (protocol == IPPROTO_AH)
-		return "AH";
-	else if (protocol == IPPROTO_MTP)
-		return "MTP";
-	else if (protocol == IPPROTO_BEETPH)
-		return "BEETPH";
-	else if (protocol == IPPROTO_ENCAP)
-		return "ENCAP";
-	else if (protocol == IPPROTO_PIM)
-		return "PIM";
-	else if (protocol == IPPROTO_COMP)
-		return "COMP";
-	else if (protocol == IPPROTO_SCTP)
-		return "SCTP";
-	else if (protocol == IPPROTO_UDPLITE)
-		return "UDPLITE";
-	else if (protocol == IPPROTO_RAW)
-		return "RAW";
+	switch (protocol) {
+	case IPPROTO_IP:
+		if (is_ipv6)
+			return "IPv6 Hop-by-Hop Options";
+		else
+			return "Dummy";
+	case IPPROTO_ICMP:
+		return "Internet Control Message Protocol (ICMP)";
+	case IPPROTO_IGMP:
+		return "Internet Group Management Protocol (IGMP)";
+	case IPPROTO_IPIP:
+		return "IPIP tunnels";
+	case IPPROTO_TCP:
+		return "Transmission Control Protocol (TCP)";
+	case IPPROTO_EGP:
+		return "Exterior Gateway Protocol (EGP)";
+	case IPPROTO_PUP:
+		return "PUP Protocol";
+	case IPPROTO_UDP:
+		return "User Datagram Protocol (UDP)";
+	case IPPROTO_IDP:
+		return "XNS IDP protocol";
+	case IPPROTO_TP:
+		return "SO Transport Protocol Class 4";
+	case IPPROTO_DCCP:
+		return "Datagram Congestion Control Protocol";
+	case IPPROTO_IPV6:
+		return "IPv6 header";
+	case IPPROTO_RSVP:
+		return "Reservation Protocol";
+	case IPPROTO_GRE:
+		return "General Routing Encapsulation";
+	case IPPROTO_ESP:
+		return "encapsulating security payload";
+	case IPPROTO_AH:
+		return "authentication header";
+	case IPPROTO_MTP:
+		return "Multicast Transport Protocol";
+	case IPPROTO_BEETPH:
+		return "IP option pseudo header for BEET";
+	case IPPROTO_ENCAP:
+		return "Encapsulation Header";
+	case IPPROTO_PIM:
+		return "Protocol Independent Multicast";
+	case IPPROTO_COMP:
+		return "Compression Header Protocol";
+	case IPPROTO_L2TP:
+		return "Layer 2 Tunnelling Protocol";
+	case IPPROTO_SCTP:
+		return "Stream Control Transmission Protocol";
+	case IPPROTO_UDPLITE:
+		return "UDP-Lite protocol";
+	case IPPROTO_MPLS:
+		return "MPLS in IP";
+	case IPPROTO_ETHERNET:
+		return "Ethernet-within-IPv6 Encapsulation";
+	case IPPROTO_RAW:
+		return "Raw IP packets";
+	case IPPROTO_MPTCP:
+		return "Multipath TCP connection";
+	}
+
+	if (!is_ipv6)
+		return "Unknown";
+
+	switch (protocol) {
+	case IPPROTO_ROUTING:
+		return "IPv6 routing header";
+	case IPPROTO_FRAGMENT:
+		return "IPv6 fragmentation header";
+	case IPPROTO_ICMPV6:
+		return "ICMPv6";
+	case IPPROTO_NONE:
+		return "IPv6 no next header";
+	case IPPROTO_DSTOPTS:
+		return "IPv6 destination options";
+	case IPPROTO_MH:
+		return "IPv6 mobility header";
+	}
+
 	return "Unknown";
 }
 
 int dissector_transport(int protocol, struct packet_info *pi,
 			const u_char *buffer, size_t len)
 {
-	if (protocol == IPPROTO_TCP)
+	switch (protocol) {
+	case IPPROTO_IP:
+		return dissector_ipv4(pi, buffer, len);
+	case IPPROTO_ICMP:
+		return dissector_icmp(pi, buffer, len);
+	case IPPROTO_TCP:
 		return dissector_tcp(pi, buffer, len);
-	else if (protocol == IPPROTO_UDP)
+	case IPPROTO_UDP:
 		return dissector_udp(pi, buffer, len);
+	case IPPROTO_IPV6:
+		return dissector_ipv6(pi, buffer, len);
+	case IPPROTO_SCTP:
+		return dissector_sctp(pi, buffer, len);
+	case IPPROTO_ETHERNET:
+		return dissector_ethernet(pi, buffer, len);
+	}
 	return 0;
 }
 
 const char *get_option(int type)
 {
 	switch (type) {
-	case 0:
+	case IPOPT_EOL:
 		return "End of Options List (EOL)";
-	case 1:
+	case IPOPT_NOP:
 		return "No Operation (NOP)";
-	case 3:
-		return "Loose Source Route (LSR)";
-	case 7:
+	case IPOPT_RR:
 		return "Record Route (RR)";
-	case 68:
+	case IPOPT_TS:
 		return "Internet Timestamp (TS)";
-	case 131:
+	case 3:
+	case IPOPT_LSRR:
+		return "Loose Source Route (LSR)";
+	case IPOPT_SATID:
+		return "Satnet ID (SATID)";
+	case IPOPT_SSRR:
 		return "Strict Source Route (SSR)";
-	case 136:
-		return "Traceroute (TR)";
+	case IPOPT_RA:
+		return "Router Alert (RA)";
 	default:
 		return "Unknown";
 	}
@@ -91,22 +141,17 @@ const char *get_option(int type)
 const char *get_class(int class)
 {
 	switch (class) {
-	case 0:
+	case IPOPT_CONTROL:
 		return "Control";
-	case 1:
+	case IPOPT_RESERVED1:
+	case IPOPT_RESERVED2:
 		return "Reserved";
-	case 2:
+	case IPOPT_MEASUREMENT:
 		return "Measurement";
-	case 3:
-		return "Reserved";
 	default:
 		return "Unknown";
 	}
 }
-
-#define OPT_TYPE_MASK 0x1f
-#define OPT_CLASS_MASK 0x60
-#define OPT_COPY_MASK 0x80
 
 int dissector_ip_options(item *options, const u_char *buffer, size_t len)
 {
@@ -115,32 +160,38 @@ int dissector_ip_options(item *options, const u_char *buffer, size_t len)
 	int count = 0;
 
 	while (len > 0) {
-		int n = count++;
-		int name_num = buffer[0] & OPT_TYPE_MASK;
-		int class_num = (buffer[0] & OPT_CLASS_MASK) >> 5;
-		const char *name = get_option(buffer[0] & OPT_TYPE_MASK);
-		const char *copy = (buffer[0] & OPT_COPY_MASK) ? "Yes" : "No";
-		const char *class = get_class(class_num);
+		int i = count++;
 
-		option = item_new_child_strf(options, "Option %d: %s", n, name);
+		int num = buffer[0] & IPOPT_NUMBER_MASK;
+		int class = buffer[0] & IPOPT_CLASS_MASK;
+		int copy = IPOPT_COPIED(buffer[0]);
+
+		const char *num_s = get_option(num);
+		const char *copy_s = copy ? "Yes" : "No";
+		const char *class_s = get_class(class);
+
+		// clang-format off
+		option = item_new_child_strf(options, "Option %d: %s", i, num_s);
 
 		type = item_new_child_strf(option, "Type: (%d)", buffer[0]);
-		item_new_child_strf(type, "Copy on fragmentation: %s", copy);
-		item_new_child_strf(type, "Class: %s (%d)", class, class_num);
-		item_new_child_strf(type, "Number: %s (%d)", name, name_num);
+		item_new_child_strf(type, "Copy on fragmentation: %s (0x%02x)", copy_s, copy);
+		item_new_child_strf(type, "Class: %s (0x%02x)", class_s, class);
+		item_new_child_strf(type, "Number: %s (0x%02x)", num_s, num);
+		// clang-format on
 
 		buffer++;
 		len--;
 
-		if (name_num == 0)
+		if (num == IPOPT_EOL)
 			break;
-		if (name_num == 1)
+		if (num == IPOPT_NOP)
 			continue;
-
+		if (buffer[0] > MAX_IPOPTLEN)
+			return -1;
 		// TODO: Parse options
 
 		// clang-format off
-		item_set_strf(option, "Options %d: %s (%i bytes)", n, name, buffer[0]);
+		item_set_strf(option, "Options %d: %s (%i bytes)", i, num_s, buffer[0]);
 		item_new_child_strf(option, "Length: %d", buffer[0]);
 		if (buffer[0] > 2)
 			item_new_child_strf(option, "Data: %s", hexdumpa(buffer + 2, buffer[0] - 2));
@@ -185,7 +236,7 @@ int dissector_ipv4(struct packet_info *pi, const u_char *buffer, size_t len)
 
 	src = strdupa(inet_ntoa(*(struct in_addr *)&ip->saddr));
 	dst = strdupa(inet_ntoa(*(struct in_addr *)&ip->daddr));
-	proto = get_protocol(ip->protocol);
+	proto = get_protocol(ip->protocol, 0);
 
 	// clang-format off
 	item = item_new_child_strf(pi->root, "IPv4, Src: %s, Dst: %s", src, dst);
@@ -220,4 +271,9 @@ int dissector_ipv4(struct packet_info *pi, const u_char *buffer, size_t len)
 	}
 
 	return dissector_transport(ip->protocol, pi, buffer, len);
+}
+
+int dissector_ipv6(struct packet_info *pi, const u_char *buffer, size_t len)
+{
+	return 0;
 }
